d06a30921690ab1aa26b008c3e2f5444015863af
MHChan <42966883+chomihee@users.noreply.github.com>
2021-09-14 15:52:14 +0900

    Add files via upload

--- season15_paralympic/paralympic_crwaler.py
+++ season15_paralympic/paralympic_crwaler.py
@@ -1,77 +1,54 @@
-import urllib.request
-import urllib.parse
-from bs4 import BeautifulSoup
 from selenium import webdriver
-import time
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
+import requests
+from tqdm import tqdm
+from pandas import json_normalize
+
 
 def paralympic_crawler():
+    # json 형태로 저장된 웹 페이지를 불러온다
+    json_url = 'https://olympics.com/tokyo-2020/paralympic-games/en/results/all-sports/zzje001a.json'
+    json_data = requests.get(json_url).json()
+
     # 옵션 생성
     options = webdriver.ChromeOptions()
     # 창 숨기는 옵션 추가
     options.add_argument("headless")
-
     driver = webdriver.Chrome('/Users/mhcho/chromedriver', options=options)
-    driver2 = webdriver.Chrome('/Users/mhcho/chromedriver', options=options)
-    driver.get('https://olympics.com/tokyo-2020/paralympic-games/en/results/all-sports/athletes.htm')
-    time.sleep(5)
+    # 해당 json에 이름 noc 관련 링크 존재함
+    paralympic_athletes = json_data['data']
 
-    # 페이지 지정
-    paralympic_athletes = {}
-    athletes_no = 1
-    # 다음 페이지 이동
-    while True:
-        if athletes_no == 100:
-            break
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        # 게시판 1번부터 19번까지 이동하면서 링크 가져오면서 이동 새 페이지 이동!
-        for i in range(0, 20):
-            temp = {}
-            link = soup.select('#entries-table > tbody >tr')[i].select('td')[0].select('a')[0]['href']
+    for athlete in tqdm(json_data['data']):
+        link = athlete['lnk']
+        driver.get('https://olympics.com/tokyo-2020/paralympic-games/en/results' + link)
 
-            name = soup.select('#entries-table > tbody >tr')[i].select('td')[0].select('a')[0].select('span')[1].text
-            print(name)
-            noc = soup.select('#entries-table > tbody >tr')[i].select('td')[1].select('a')[0]['title'].split('\n')[1]
-            driver2.get('https://olympics.com/tokyo-2020/paralympic-games/en/results' + link)
-            time.sleep(5)
-            temp['name'] = name
-            temp['NOC'] = noc
-            # 이동한 페이지에 정보를 긁어옴
-            #  정보 긁어옴
-            basic_infos = driver2.find_elements_by_class_name('col-md-6')
+        # 이동한 페이지에 정보를 긁어옴
+        # 동작 도중 index error 발생한 경우 그냥 pass 진행 > 해당번호(1번부터시작) 1320, 1912, 2864 에서 오류 발생 csv 직접 수정..?
+        try:
+            basic_infos = driver.find_elements_by_class_name('col-md-6')
             for basic_info in basic_infos[0].find_elements_by_tag_name('div'):
                 if basic_info.text != "":
-                    temp[basic_info.text.split(': ')[0]] = basic_info.text.split(': ')[1]
+                    athlete[basic_info.text.split(':')[0]] = basic_info.text.split(':')[1]
 
-            temp[basic_infos[1].text.split(': ')[0]] = basic_infos[1].text.split(': ')[1]
+            for basic_info in basic_infos[1].find_elements_by_tag_name('div'):
+                if (basic_info.text != "") and (":" in basic_info.text):
+                    athlete[basic_info.text.split(':')[0]] = basic_info.text.split(':')[1]
 
-            # 추가 정보
-            add_infos = driver2.find_elements_by_class_name('form-group')  # 길이만큼 가져와야함
-            for info in add_infos:
-                if ':' in info.text.split('\n')[0]:
-                    label = info.text.split('\n')[0].replace(":", "")
-                else:
-                    label = info.text.split('\n')[0]
-                temp[label] = info.text.split('\n')[1]
+        except IndexError:
+            print("Index error link : " + link)
+            pass
 
-            paralympic_athletes[athletes_no] = temp
-            print(athletes_no)
-            athletes_no = athletes_no + 1
-
-        # 오류날때까지 페이지 이동
-        # 현재 페이지가 안넘어가는 이슈가 있음 !!
-        # // *[ @ id = "entries-table_paginate"]
-        # driver.find_elements_by_css_selector('ul.pagination > *')[8].click().perform()
-        #element = driver.find_element_by_xpath('// *[ @ id = "entries-table_paginate"]')
-        element = driver.find_elements_by_css_selector('ul.pagination > *')[8]
-        actions = ActionChains(driver)
-        actions.move_to_element(element).click().perform()
-
-
+        # 추가 정보
+        add_infos = driver.find_elements_by_class_name('form-group')  # 길이만큼 가져와야함
+        for info in add_infos:
+            if ':' in info.text.split('\n')[0]:
+                label = info.text.split('\n')[0].replace(":", "")
+            else:
+                label = info.text.split('\n')[0]
+            athlete[label] = info.text.split('\n')[1]
+    driver.close()
     return paralympic_athletes
 
 
 if __name__ == "__main__":
-    print(paralympic_crawler())
+    df = json_normalize(paralympic_crawler())
+    df.to_csv('./data/paralympic_athletes.csv')
